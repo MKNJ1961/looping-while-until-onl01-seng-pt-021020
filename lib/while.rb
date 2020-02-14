d427ef568d7c0f29a483ba06392c20018249bbe6
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-02-14 01:42:55 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,9 +1,9 @@
 counter = 10
-def using_while
-  levitation_force = 6
-  puts "Wingardium Lavosia"
-  using_while += 1
-end
+  def using_while
+    levitation_force = 6
+    puts "Wingardium Lavosia"
+    using_while += 1
+  end
 end
  
   
